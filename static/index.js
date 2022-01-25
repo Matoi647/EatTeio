8b18cb6447a19543b1dc4d0076cbcc3924d7b730
Matoi647 <96243433+Matoi647@users.noreply.github.com>
2022-01-25 19:05:50 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,9 +341,9 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return 'Hachimi';
-    if (score <= 99) return '东海帝王';
-    if (score <= 149) return '东帝海王';
+    if (score <= 49) return '牡~蛎~~';
+    if (score <= 99) return 'Hachimi';
+    if (score <= 149) return '东海帝王';
     if (score <= 199) return '东海龙王';
     return '人？';
 }
